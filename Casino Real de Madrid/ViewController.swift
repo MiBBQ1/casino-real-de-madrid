feefa684c5598abc60f4b3c033bf41933f8c76d3
m-deriglazov <30551142+m-deriglazov@users.noreply.github.com>
2025-02-10 11:26:24 +0200

    Initial Commit

---
+++ Casino Real de Madrid/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Casino Real de Madrid
+//
+//  Created by geozilla on 10.02.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
